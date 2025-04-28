107a55578b994e3cd4d1da467a2139a61fc6532f
waiyarti <113837151+waiyarti@users.noreply.github.com>
2025-04-28 10:05:15 +0200

    Update GraphiqueNote.js

--- components/GraphiqueNote.js
+++ components/GraphiqueNote.js
@@ -1,11 +1,11 @@
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 export default function GraphiqueNote({ historique }) {
-  const historiqueInverse = historique.slice().reverse(); // ==> Très important
+  const historiqueInverse = historique.slice().reverse(); // ➔ pour que ce soit dans le bon sens
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-md mt-8">
-      <h2 className="text-xl font-bold mb-4">✏️ Évolution des notes</h2>
+      <h2 className="text-xl font-bold mb-4">✏️ Évolution de la note sur 20</h2>
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={historiqueInverse} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
@@ -13,7 +13,7 @@ export default function GraphiqueNote({ historique }) {
           <YAxis domain={[0, 20]} />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="note" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="note" stroke="#f97316" strokeWidth={3} />
         </LineChart>
       </ResponsiveContainer>
     </div>
