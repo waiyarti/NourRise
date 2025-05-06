9297e8ef6d50e38ad939469647273d864c859eb2
waiyarti <113837151+waiyarti@users.noreply.github.com>
2025-05-06 09:34:39 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,9 @@
-// Fichier : supabaseClient.js
+// --- Importation de la librairie officielle Supabase ---
 import { createClient } from '@supabase/supabase-js'
 
-// Ton URL et ta clé publique (anon key) depuis Supabase
+// --- Informations de ton projet (OK à copier tel quel) ---
 const supabaseUrl = 'https://iukagcbgrqtscomsvnso.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1a2FnY2JncnF0c2NvbXN2bnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMTgzNDYsImV4cCI6MjA2MTU5NDM0Nn0.0iaVcyQ_j8E_Zp2_XyVR87KSll20RywoA3G7CqtGwo4'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1a2FnY2JncnF0c2NvbXN2bnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjA5NDksImV4cCI6MjA2MjAzNjk0OX0.Rx80_BW9dyG9rn9PBbYlK9kr3atDUuOsMtIYHIJrzzc'
 
+// --- Création du client Supabase (connexion à ton projet) ---
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
